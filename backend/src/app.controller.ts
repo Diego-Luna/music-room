f9b3435c675e8452b4d3508164e6f55cfe38bf1d
jecointr <104733115+jecointr@users.noreply.github.com>
2026-05-14 12:53:50 +0100

    fix(api): make root route public for redirection

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
+import { Public } from './common/decorators/public.decorator';
 
 @Controller()
 export class AppController {
+  @Public()
   @Get()
   @ApiExcludeEndpoint()
   @Redirect('/api/docs', 301)
