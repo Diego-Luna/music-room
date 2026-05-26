3eb26191318db1ad80f3d31ae98f7d4e4412d24d
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2026-05-26 14:08:00 +0200

    Add debug print statement in MyApp class

--- frontend/music_room_app/lib/main.dart
+++ frontend/music_room_app/lib/main.dart
@@ -18,6 +18,7 @@ void main() async {
   setupLocator();
   // await HiveConfig.init();
   await authProvider.tryAutoLogin();
+  
   runApp(const AppState());
 }
 
@@ -32,6 +33,7 @@ class AppState extends StatelessWidget {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  print('helo');
 
   @override
   Widget build(BuildContext context) {
